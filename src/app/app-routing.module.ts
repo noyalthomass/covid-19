099606a2876a8a4ed0cbd2974f06e1de1321d34d
Adarsh Sathyan <adarsh.sathyan@ftsindia.in>
2022-01-13 15:14:51 +0530

    docs:created the base architeture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesComponent } from './countries/countries.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditCountryComponent } from './edit-country/edit-country.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'home',component:DashboardComponent},
+  {path:'countries',component:CountriesComponent},
+  {path:'edit-country',component:EditCountryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountriesComponent } from './countries/countries.component';
+import { EditCountryComponent } from './edit-country/edit-country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    DashboardComponent,
+    CountriesComponent,
+    EditCountryComponent
   ],
   imports: [
     BrowserModule,
