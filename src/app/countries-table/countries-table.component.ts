7fa4da12d8d082339743e0d62a3220dbd0523827
Adarsh Sathyan <adarsh.sathyan@ftsindia.in>
2022-01-20 17:35:06 +0530

    feat:listed covid details in material table

--- src/app/countries-table/countries-table.component.ts
+++ src/app/countries-table/countries-table.component.ts
@@ -1,61 +1,74 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { ReplaySubject, takeUntil } from 'rxjs';
-import {Countries} from '../models/index'
+import { Countries } from '../models/index';
 import { DataService } from '../services/data.service';
-import { NgxSpinnerService } from "ngx-spinner";
-
+import { NgxSpinnerService } from 'ngx-spinner';
 
 const ELEMENT_DATA: Countries[] = [];
 @Component({
   selector: 'app-countries-table',
   templateUrl: './countries-table.component.html',
-  styleUrls: ['./countries-table.component.scss']
+  styleUrls: ['./countries-table.component.scss'],
 })
-
-
-
 export class CountriesTableComponent implements OnInit {
-
-  displayedColumns: string[] = ['title', 'flag', 'cases', 'deaths','recovered','tests','population'];
-  dataSource:any
-
+  displayedColumns: string[] = [
+    'title',
+    'flag',
+    'cases',
+    'deaths',
+    'recovered',
+    'tests',
+    'population',
+  ];
+  dataSource: any;
   destroy$: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
-
+  @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
- 
-  constructor(private service:DataService,private spinner: NgxSpinnerService) { }
+  constructor(
+    private service: DataService,
+    private spinner: NgxSpinnerService,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
 
   ngOnInit(): void {
-    this.loadData()
-  
+    this.loadData();
   }
 
-  loadData(){
-    this.spinner.show()
-    this.service.getCountriesData().pipe(
-      takeUntil(this.destroy$)
-    )
-    .subscribe((res:any)=>{
-      res.forEach((country:any)=>{
-        ELEMENT_DATA.push({
-          cases:country.cases,
-          title:country.country,
-          flag:country.countryInfo.flag,
-          deaths:country.deaths,
-          recovered:country.recovered,
-          tests:country.tests,
-          population:country.population,
-          updated:country.updated,
-        })
-      })
-      this.dataSource= new MatTableDataSource<Countries>(ELEMENT_DATA);
-      this.dataSource.paginator = this.paginator;
-      this.spinner.hide()
-    })
-
+  loadData() {
+    this.spinner.show();
+    this.service
+      .getCountriesData()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: any) => {
+        res.forEach((country: any) => {
+          ELEMENT_DATA.push({
+            cases: country.cases,
+            title: country.country,
+            flag: country.countryInfo.flag,
+            deaths: country.deaths,
+            recovered: country.recovered,
+            tests: country.tests,
+            population: country.population,
+            updated: country.updated,
+          });
+        });
+        this.dataSource = new MatTableDataSource<Countries>(ELEMENT_DATA);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.spinner.hide();
+      });
+  }
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
